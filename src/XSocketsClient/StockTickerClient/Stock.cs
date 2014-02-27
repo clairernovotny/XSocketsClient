118ec2c10d260c5ec60eff3a65397d6c066eb405
Oren Novotny <oren@novotny.org>
2014-02-27 17:11:27 -0500

    Add a test client

---
+++ src/XSocketsClient/StockTickerClient/Stock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockTickerClient
+{
+    public class Stock : IEquatable<Stock>
+    {
+        public bool Equals(Stock other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Symbol, other.Symbol);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Stock)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Symbol != null ? Symbol.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Stock left, Stock right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Stock left, Stock right)
+        {
+            return !Equals(left, right);
+        }
+
+        public string Symbol { get; set; }
+        public decimal DayOpen { get; set; }
+        public decimal DayClose { get; set; }
+        public decimal DayLow { get; set; }
+        public decimal DayHigh { get; set; }
+        public decimal LastChange { get; set; }
+        public decimal Change { get; set; }
+        public double PercentChange { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/XSocketsClient/StockTickerClient/StockListViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using XSocketsClient;
+using XSocketsClient.Common.Event.Interface;
+
+namespace StockTickerClient
+{
+    public class StockListViewModel
+    {
+        private readonly SynchronizationContext _context;
+        public ObservableCollection<Stock> Stocks { get; private set; }
+
+        private readonly XSocketClient _client;
+        
+        public StockListViewModel()
+        {
+            _context = SynchronizationContext.Current;
+
+            Stocks = new ObservableCollection<Stock>();
+
+            _client = new XSocketClient("ws://localhost.fiddler:15820/Stock", "*");
+            
+        }
+
+        public async void Start()
+        {
+            await _client.Open();
+            Debug.WriteLine("Connection Open");
+            await _client.Bind("allStocks", OnAllStocks);
+            await _client.Bind("tick", OnTick);
+            Debug.WriteLine("Listening for ticks");
+        }
+
+        private void OnAllStocks(ITextArgs args)
+        {
+            Debug.WriteLine("OnAllStocks");
+            var stocks = JsonConvert.DeserializeObject<IList<Stock>>(args.data);
+
+            RunOnContext(() =>
+            {
+                foreach (var stock in stocks)
+                    AddOrUpdateStock(stock);
+            });
+        }
+
+        private void OnTick(ITextArgs args)
+        {
+            Debug.WriteLine("Tick");
+
+            var stock = JsonConvert.DeserializeObject<Stock>(args.data);
+
+            RunOnContext(() => AddOrUpdateStock(stock));
+        }
+
+        private void AddOrUpdateStock(Stock stock)
+        {
+
+            var i = Stocks.IndexOf(stock);
+            if (i >= 0)
+            {
+                Stocks[i] = stock;
+            }
+            else
+            {
+                Stocks.Add(stock);
+            }
+        }
+
+        private void RunOnContext(Action action)
+        {
+            if (_context != null)
+            {
+                _context.Post(_ => action(), null);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
