9a85282b14a5bde0c3e9f78a38ce212645d2444e
Oren Novotny <oren@novotny.org>
2014-02-27 15:29:13 -0500

    Update sample to use MVC controller

--- src/XSocketsClient/Samples.Web/App_Start/XSocketsBootstrap.cs
+++ src/XSocketsClient/Samples.Web/App_Start/XSocketsBootstrap.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Web;
+using XSockets.Plugin.Framework;
+using XSockets.Server;
 
 [assembly: PreApplicationStartMethod(typeof(Samples.Web.App_Start.XSocketsBootstrap), "Start")]
 
@@ -13,19 +15,23 @@ namespace Samples.Web.App_Start
     //Create class for the server instance
     public static class XSocketsBootstrap
     {
-        private static IXSocketServerContainer container;
+        //private static IXSocketServerContainer container;
+        private static IXSocketsRelayContainer container;
         public static void Start()
         {
-            container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();
-            container.StartServers();
-            foreach (var server in container.Servers)
-            {
-                Debug.WriteLine("Started Server: {0}:{1}", server.ConfigurationSetting.Uri.Host, server.ConfigurationSetting.Port);
-                Debug.WriteLine(string.Format("Scheme: {0}", server.ConfigurationSetting.Uri.Scheme));
-                Debug.WriteLine("SSL/TLS: {0}", server.ConfigurationSetting.IsSecure);
-                Debug.WriteLine("Allowed Connections (0 = infinite): {0}", server.ConfigurationSetting.NumberOfAllowedConections);
-                Debug.WriteLine("------------------------------------------------------");
-            }
+
+            //container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();
+            container = Composable.GetExport<IXSocketsRelayContainer>();
+            container.Start();
+            //container.StartServers();
+            //foreach (var server in container.Servers)
+            //{
+            //    Debug.WriteLine("Started Server: {0}:{1}", server.ConfigurationSetting.Uri.Host, server.ConfigurationSetting.Port);
+            //    Debug.WriteLine(string.Format("Scheme: {0}", server.ConfigurationSetting.Uri.Scheme));
+            //    Debug.WriteLine("SSL/TLS: {0}", server.ConfigurationSetting.IsSecure);
+            //    Debug.WriteLine("Allowed Connections (0 = infinite): {0}", server.ConfigurationSetting.NumberOfAllowedConections);
+            //    Debug.WriteLine("------------------------------------------------------");
+            //}
         }
     }
 }
---
+++ src/XSocketsClient/Samples.Web/Controllers/StockController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XSockets.MVC;
+
+namespace Samples.Web.Controllers
+{
+    public class StockController : XSocketsControllerBase<StockTickerSample.Controllers.StockController>
+    {
+        //
+        // GET: /Stock/
+        public ActionResult Index()
+        {
+            return HttpNotFound("This endpoint is only available via WebSockets");
+        }
+	}
+}
