7821c7d82bb34db3b5ae3292c00029bbd2c95093
Oren Novotny <oren@novotny.org>
2014-02-27 15:31:31 -0500

    Cleanup the link

--- src/XSocketsClient/Samples.Web/Global.asax.cs
+++ src/XSocketsClient/Samples.Web/Global.asax.cs
@@ -5,13 +5,20 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using XSockets.Plugin.Framework;
+using XSockets.Server;
 
 namespace Samples.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private IXSocketsRelayContainer _container;
         protected void Application_Start()
         {
+            // Start the MVC->XSockets bridge
+            _container = Composable.GetExport<IXSocketsRelayContainer>();
+            _container.Start();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
